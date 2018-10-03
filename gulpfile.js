af51507793e4ec8d38560ef93d8dc03b297e1533
Keks <keks@htmlacademy.ru>
2018-10-03 09:32:22 +0300

    :kissing_cat: Добавляет сборку Sass на Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,34 @@
-var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    browserSync = require('browser-sync'),
-    cssmin = require('gulp-cssmin'),
-    rename = require('gulp-rename'),
-    sourcemaps = require('gulp-sourcemaps'),
-    plumber = require('gulp-plumber');
+"use strict";
 
-gulp.task('sass', function(){
-    return gulp.src('app/sass/**/*.sass')
-        .pipe(sass())
-        .pipe(gulp.dest('app/css'))
-        .pipe(browserSync.reload({stream: true}))
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var plumber = require("gulp-plumber");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var server = require("browser-sync").create();
+
+gulp.task("css", function () {
+  return gulp.src("source/sass/style.scss")
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(gulp.dest("source/css"))
+    .pipe(server.stream());
 });
 
-gulp.task('browser-sync', function() { // Создаем таск browser-sync
-    browserSync({ // Выполняем browser Sync
-        server: { // Определяем параметры сервера
-            baseDir: 'app' // Директория для сервера - app
-        },
-        notify: false // Отключаем уведомления
-    });
+gulp.task("server", function () {
+  server.init({
+    server: "source/",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
+  gulp.watch("source/*.html").on("change", server.reload);
 });
 
-gulp.task('default', function () {
-    gulp.src('app/**/*.css')
-        .pipe(plumber())
-        .pipe(sourcemaps.init())
-            .pipe(cssmin())
-            .pipe(rename({suffix: '.min'}))
-        .pipe(sourcemaps.write())    
-        .pipe(gulp.dest('dist'));
-});
-
-gulp.task('watch', ['browser-sync', 'sass', 'default'], function() {
-    gulp.watch('app/sass/**/*.sass', ['sass']);
-    gulp.watch('app/*.html', browserSync.reload);
-});
+gulp.task("start", gulp.series("css", "server"));
